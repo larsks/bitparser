3e0e06277890e401b63b691271320343cb6dd830
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-03-31 14:16:41 -0400

    Docs, buffers, bitfields, and more
    - Added BufferedReader for providing peek() and pushpack() operations on
      a stream.
    - Added BitField field.
    - Added some docstrings.

--- bitparser.py
+++ bitparser.py
@@ -1,7 +1,54 @@
 import os
 import sys
-
 import struct
+
+class EndOfData (Exception):
+    '''Raised when we run out of data to parse.  This does not
+    necessarily mean we have reached EOF.'''
+    pass
+
+class BufferedReader (object):
+    '''Provides peek() and pushback() methods for a Python file object.'''
+
+    def __init__(self, fd, bufsize=4096):
+        self._read = fd.read
+        self.held = ''
+        self.eof = False
+        self.bufsize=bufsize
+
+    def read(self, length):
+        '''Return no more than length bytes of data to the caller.'''
+
+        assert length > 0
+
+        while len(self.held) < length and not self.eof:
+            chunk = self._read(self.bufsize)
+            if chunk:
+                self.held += chunk
+            else:
+                self.eof = True
+
+        if len(self.held) > length:
+            self.held, result = (self.held[length :],
+                    self.held[: length])
+        else:
+            self.held, result = '', self.held
+
+        return result
+
+    def pushback(self, bytes):
+        '''Push bytes back onto the buffer to be returned by a 
+        subsequent read() operation.'''
+
+        self.held = bytes + self.held
+
+    def peek(self, length=1):
+        '''Return the next length bytes in the buffer.  These bytes will 
+        be returned by a subsequent read() operation.'''
+
+        bytes = self.read(length)
+        self.pushback(bytes)
+        return data
 
 class Container(dict):
     def __init__ (self, struct, *args, **kwargs):
@@ -9,15 +56,38 @@ class Container(dict):
         self.__struct__ = struct
 
     def pack(self):
+        '''Return the binary representation of this object.'''
+
         return self.__struct__.pack(self)
+
+    def write(self, fd):
+        '''Write the binary representation of this object to
+        a file.'''
+
+        fd.write(self.pack())
 
 class Struct (object):
     def __init__ (self, *fields, **kwargs):
         self.fields = fields
         self.factory = kwargs.get('factory', Container)
 
+    def size(self):
+        '''Returns the minimum size of a Struct.  Variable length
+        fields (e.g., CStrings) or repeating elements may result in
+        a size larger than the number returned by this method.'''
+
+        return sum(f.size() for f in self.fields)
+
     def unpack(self, fd):
+        '''Convert a binary stream into structured data.'''
+
         values = self.factory(self)
+
+        data = fd.read(self.size())
+        fd.pushback(data)
+
+        if len(data) < self.size():
+            raise EndOfData()
 
         for f in self.fields:
             val = f.unpack(fd)
@@ -27,6 +97,8 @@ class Struct (object):
         return values
 
     def pack(self, values):
+        '''Convert structured data into a binary stream.'''
+
         bytes = []
 
         for f in self.fields:
@@ -37,6 +109,9 @@ class Struct (object):
         return ''.join(bytes)
 
 class Alias(object):
+    '''Embed an anonymous Struct inside another struct by given it a name
+    and default value.'''
+
     def __init__ (self, struct, name, default=None):
         self.name = name
         self.struct = struct
@@ -50,6 +125,8 @@ class Alias(object):
         return getattr(self.struct, k)
 
 class Array (object):
+    '''A fixed-length, multiple-value field.'''
+
     def __init__ (self, name, format, default=None):
         self.name = name
         self.struct = struct.Struct(format)
@@ -59,8 +136,14 @@ class Array (object):
 
         self.default = default
 
+    def read(self, fd, size):
+        bytes = fd.read(size)
+        if bytes == '':
+            raise EndOfData()
+        return bytes
+
     def unpack(self, fd):
-        bytes = fd.read(self.size())
+        bytes = self.read(fd, self.size())
         data = self.struct.unpack(bytes)
         return data
 
@@ -73,6 +156,8 @@ class Array (object):
 
 
 class Field (Array):
+    '''A fixed-length, single-value field.'''
+
     def __init__(self, name, format, default=0):
         super(Field, self).__init__(name, format, default=default)
 
@@ -83,6 +168,10 @@ class Field (Array):
         return super(Field, self).pack((data,))
 
 class Constant (Field):
+    '''A constant value.  The consant value will always be written
+    out regardless of the value passed to pack().  The unpack() method will
+    raise ValueError if the value read from the stream does not match the
+    constant value.'''
 
     def __init__(self, name, format, value, **kwargs):
         super(Constant, self).__init__(name, format, **kwargs)
@@ -98,4 +187,58 @@ class Constant (Field):
             raise ValueError('constant %s expected %s, got %s' %
                     (self.name, repr(self.value), repr(data)))
         return data
+
+class CString (Field):
+    '''A NUL (or other character) delimited variable-length field.'''
+    
+    def __init__ (self, name, delim='\x00', default='', **kwargs):
+        super(CString, self).__init__(name, '', default=default,
+                **kwargs)
+        self.delim = delim
+
+    def pack(self, data):
+        return '%s%s' % (data, self.delim)
+
+    def unpack(self, fd):
+        bytes = []
+
+        while True:
+            b = self.read(fd, 1)
+            if b == self.delim:
+                break
+            bytes.append(b)
+
+        return ''.join(bytes)
+
+    def size(self):
+        return 0
+
+class BitField (Field):
+    '''Treat an integer field as a list of boolean values.'''
+
+    def __init__(self, name, format, fields, **kwargs):
+        super(Field, self).__init__(name, format, **kwargs)
+        self.fields = fields
+
+    def unpack(self, fd):
+        data = super(Field, self).unpack(fd)[0]
+        values = {}
+
+        for i,f in enumerate(self.fields):
+            if f is None:
+                continue
+            mask = 1 << i
+            values[f] = bool(data & mask)
+
+        return values
+
+    def pack(self, values):
+        data = 0
+        for i,f in enumerate(self.fields):
+            if f is None:
+                continue
+            if values[f]:
+                data = data | (1<<i)
+
+        return super(Field, self).pack((data,))
 
